acd3c4b8f8196adb678c2fcce739b169dce124b2
Yovieaji13 <71000479+Yovieaji13@users.noreply.github.com>
2021-11-19 10:00:11 +0700

    commit

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "morfee";
+$db = "morfeen";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
